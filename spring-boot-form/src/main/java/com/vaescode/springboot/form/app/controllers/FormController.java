156cc063993ca034130077bcc2dfda426bd3e2da
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-01-14 20:16:11 -0600

    Form: Validación atributo object - Thymeleaf

--- spring-boot-form/src/main/java/com/vaescode/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/vaescode/springboot/form/app/controllers/FormController.java
@@ -29,12 +29,12 @@ public class FormController {
 		
 		model.addAttribute("titulo", "Resultado del form");
 		if (result.hasErrors()) {
-			Map<String, String> errores = new HashMap<>();
+			/*Map<String, String> errores = new HashMap<>();
 			result.getFieldErrors().forEach(err -> {
 				errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
 			});
 			
-			model.addAttribute("error", errores);
+			model.addAttribute("error", errores);*/
 			return "form";
 		}
 		
