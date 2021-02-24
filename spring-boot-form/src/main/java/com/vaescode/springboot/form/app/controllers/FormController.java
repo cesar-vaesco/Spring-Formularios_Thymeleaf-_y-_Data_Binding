51ca0ae529ef00a746e6199932cede739af98ada
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-02-23 20:33:46 -0600

    Form: implementando checkboxes con Map<>

--- spring-boot-form/src/main/java/com/vaescode/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/vaescode/springboot/form/app/controllers/FormController.java
@@ -73,6 +73,15 @@ public class FormController {
 		return roles;
 	}
 
+	@ModelAttribute("listaRolesMap")
+	public Map<String, String> rolesMap(){
+		Map<String, String> roles = new HashMap<String, String>();
+		roles.put("ROLE_ADMIN", "Administrador");
+		roles.put("ROLE_USER", "Usuario");
+		roles.put("ROLE_MODERATOR", "Moderador");
+		return roles;
+	}
+	
 	@ModelAttribute("paises")
 	public List<String> paises() {
 		return Arrays.asList("México", "España", "Chile", "Argentina", "Ecuador", "Perú", "Colombia");
