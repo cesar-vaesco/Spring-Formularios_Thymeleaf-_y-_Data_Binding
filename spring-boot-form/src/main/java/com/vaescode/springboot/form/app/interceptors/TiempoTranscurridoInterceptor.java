6b2b56fc9d3477af5f6fe9464bb56a8deca42095
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-02-25 13:10:55 -0600

    Form-interceptores HTTP: mostrando método handler que consume la vista

--- spring-boot-form/src/main/java/com/vaescode/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
+++ spring-boot-form/src/main/java/com/vaescode/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -21,6 +22,14 @@ public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		
+		
+		//Validación 
+				if(handler instanceof HandlerMethod) {
+					HandlerMethod metodo = (HandlerMethod)handler;
+					logger.info("es un método del controlador: " + metodo.getMethod().getName());
+				}
+				
 		logger.info("TiempoTranscurridoInterceptor: preHandle() entrando...");
 		logger.info("Interceptando: " + handler);
 		long tiempoInicio = System.currentTimeMillis();
@@ -42,9 +51,11 @@ public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
 		long tiempoInicio = (Long) request.getAttribute("tiempoInicio");
 		long tiempoTranscurrido = tiempoFin - tiempoInicio;
 		
-		if(modelAndView != null) {
+		//Validación 
+		if(handler instanceof HandlerMethod && modelAndView != null) {
 			modelAndView.addObject("tiempoTranscurrido",tiempoTranscurrido);
 		}
+		
 		logger.info("Tiempo Transcurrido: " + tiempoTranscurrido + "milisegundos");
 		logger.info("TiempoTranscurridoInterceptor: postHandle() saliendo...");
 	}
