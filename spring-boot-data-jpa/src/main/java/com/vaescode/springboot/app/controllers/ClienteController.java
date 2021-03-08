d74a3b900aad9e5a40d414423f89faf0789f314f
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-03-08 11:25:53 -0600

    JPA-MVC: vista - listar - datos de prueba en import.sql

--- spring-boot-data-jpa/src/main/java/com/vaescode/springboot/app/controllers/ClienteController.java
+++ spring-boot-data-jpa/src/main/java/com/vaescode/springboot/app/controllers/ClienteController.java
@@ -1,21 +1,20 @@
 package com.vaescode.springboot.app.controllers;
 
-import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.vaescode.springboot.app.models.dao.IClienteDao;
-import com.vaescode.springboot.app.models.entity.Cliente;
+
 
 @Controller
 public class ClienteController {
 	
 	@Autowired
-	@Qualifier
 	private IClienteDao clienteDao;
 	
 	
---
+++ spring-boot-data-jpa/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+/*Poblando tabla*/
+
+insert into clientes (id, nombre, apellido, email, create_at) values (1, 'César', 'Vargas', 'cesar@gmail.com', '2020-05-25');
+insert into clientes (id, nombre, apellido, email, create_at) values (2, 'Verónica', 'Cortez', 'vero@gmail.com', '2020-06-17');
