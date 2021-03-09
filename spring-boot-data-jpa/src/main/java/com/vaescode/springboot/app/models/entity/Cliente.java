73ad295cadc26f47ae54cca9af5df7a7d1dbdf76
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-03-09 13:20:05 -0600

    JPA-MVC: campo fecha - configurando en clase entity y en vista form.html

--- spring-boot-data-jpa/src/main/java/com/vaescode/springboot/app/models/entity/Cliente.java
+++ spring-boot-data-jpa/src/main/java/com/vaescode/springboot/app/models/entity/Cliente.java
@@ -8,10 +8,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name = "clientes")
@@ -25,14 +26,17 @@ public class Cliente implements Serializable {
 	private String apellido;
 	private String email;
 
+	
 	@Column(name = "create_at")
+	@DateTimeFormat(pattern ="yyyy-MM-dd")
 	@Temporal(TemporalType.DATE)
 	private Date createAt;
-
+	
+	/*
 	@PrePersist
 	public void prePersist() {
 		createAt = new Date();
-	}
+	}*/
 
 	public Long getId() {
 		return id;
--- spring-boot-data-jpa/src/main/resources/import.sql
+++ spring-boot-data-jpa/src/main/resources/import.sql
@@ -2,3 +2,4 @@
 
 insert into clientes (id, nombre, apellido, email, create_at) values (1, 'César', 'Vargas', 'cesar@gmail.com', '2020-05-25');
 insert into clientes (id, nombre, apellido, email, create_at) values (2, 'Verónica', 'Cortez', 'vero@gmail.com', '2020-06-17');
+insert into clientes (id, nombre, apellido, email, create_at) values (3, 'Hugo', 'Vargas', 'hugo@gmail.com', '2020-05-08');
