8c1ff6b4aee6663193f96fb711c0ab542d3d5db1
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-03-09 18:52:54 -0600

    JPA-MVC: agregando funcionalidad para editar datos del cliente

--- spring-boot-data-jpa/src/main/java/com/vaescode/springboot/app/controllers/ClienteController.java
+++ spring-boot-data-jpa/src/main/java/com/vaescode/springboot/app/controllers/ClienteController.java
@@ -1,7 +1,5 @@
 package com.vaescode.springboot.app.controllers;
 
-
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,49 +8,75 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.vaescode.springboot.app.models.dao.IClienteDao;
 import com.vaescode.springboot.app.models.entity.Cliente;
 
-
 @Controller
+@SessionAttributes("cliente")
 public class ClienteController {
-	
+
 	@Autowired
 	private IClienteDao clienteDao;
+
 	
 	
 	@GetMapping("/listar")
-	public String listar(Model model){
+	public String listar(Model model) {
 		model.addAttribute("titulo", "Listado de clientes");
 		model.addAttribute("clientes", clienteDao.findAll());
 		return "listar";
 	}
+
+	
 	
 	@GetMapping("/form")
 	public String crear(Model model) {
-		
+
 		Cliente cliente = new Cliente();
 		model.addAttribute("cliente", cliente);
 		model.addAttribute("titulo", "Formulario de cliente");
-		
-		return "form"; 
+
+		return "form";
 	}
+
 	
-	@PostMapping("/form")
-	public String guardar (@Valid Cliente cliente, BindingResult result, Model model) {
+	
+	@RequestMapping(value= "/form/{id}")
+	public String editar(@PathVariable(value="id") Long id, Model model) {
 		
-		if( result.hasErrors()) {
+		Cliente cliente = null;
+		
+		if(id > 0) {
+			cliente = clienteDao.findOne(id);
+		} else {
+			return "redirect:/listar";
+		}
+		
+		model.addAttribute("cliente", cliente);
+		model.addAttribute("titulo", "Editar cliente");
+		
+		return "form";
+	}
+
+	@PostMapping("/form")
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
+
+		if (result.hasErrors()) {
 			model.addAttribute("titulo", "Formulario de cliente");
 			return "form";
 		}
-		
+
 		clienteDao.save(cliente);
+		/*Elimina el objeto cliente de la sesión y acompleta la acción de guardar - insertar */
+		status.setComplete();
+		
 		return "redirect:listar";
 	}
-	
-	
-	
 
 }
--- spring-boot-data-jpa/src/main/java/com/vaescode/springboot/app/models/dao/ClienteDaoImpl.java
+++ spring-boot-data-jpa/src/main/java/com/vaescode/springboot/app/models/dao/ClienteDaoImpl.java
@@ -5,19 +5,17 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.vaescode.springboot.app.models.entity.Cliente;
 
 @Repository()
-public class ClienteDaoImpl implements IClienteDao{
+public class ClienteDaoImpl implements IClienteDao {
 
 	@PersistenceContext
-	private EntityManager em; 
-	
-	
+	private EntityManager em;
+
 	@SuppressWarnings("unchecked")
 	@Transactional(readOnly = true)
 	@Override
@@ -25,11 +23,24 @@ public class ClienteDaoImpl implements IClienteDao{
 		return em.createQuery("from Cliente").getResultList();
 	}
 
-
 	@Override
 	@Transactional
 	public void save(Cliente cliente) {
-		em.persist(cliente);
+		if (cliente.getId() != null && cliente.getId() > 0) {
+			/*
+			 * el método merge une/actualiza los datos actualizados */
+			em.merge(cliente);
+		} else {
+			/*
+			 * MAntiene los datos del cliente*/
+			em.persist(cliente);
+		}
+
+	}
+
+	@Override
+	public Cliente findOne(Long id) {
+		return em.find(Cliente.class, id);
 	}
 
 }
